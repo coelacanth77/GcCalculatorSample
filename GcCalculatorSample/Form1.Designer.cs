da34144001ea3ead5a6f2fb54c06b554e23a58a3
Makoto Nishimura <mac.nishimura@gmail.com>
2016-09-13 01:34:31 +0900

    プロジェクト ファイルを追加します。

---
+++ GcCalculatorSample/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GcCalculatorSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gcCalculator1 = new GrapeCity.Win.Calculations.GcCalculator();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gcCalculator1
+            // 
+            this.gcCalculator1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.gcCalculator1.Location = new System.Drawing.Point(12, 23);
+            this.gcCalculator1.Name = "gcCalculator1";
+            this.gcCalculator1.Size = new System.Drawing.Size(180, 180);
+            this.gcCalculator1.TabIndex = 0;
+            this.gcCalculator1.OutputTextChanged += new System.EventHandler(this.gcCalculator1_OutputTextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "値は";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gcCalculator1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GrapeCity.Win.Calculations.GcCalculator gcCalculator1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
